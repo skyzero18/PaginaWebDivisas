2ec2c572b699fca0aea82dd6663b500f3253d9e6
skyzero18 <lucianojofre122@gmail.com>
2024-09-28 22:42:34 -0300

    Primer commit

---
+++ PaginaWebDivisas/src/main/java/com/example/PaginaWebDivisas/PaginaWebDivisasApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaginaWebDivisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebDivisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebDivisasApplication.class, args);
+	}
+
+}
---
+++ PaginaWebDivisas/src/test/java/com/example/PaginaWebDivisas/PaginaWebDivisasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaginaWebDivisas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebDivisasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
