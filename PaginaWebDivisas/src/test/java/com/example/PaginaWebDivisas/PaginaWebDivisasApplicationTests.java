3c284eefdc140164deb314e3c399eed75860d587
XyenzaX <juanpablollensa2@gmail.com>
2024-10-25 20:43:05 -0300

    v4.1 logs

--- PaginaWebDivisas/src/test/java/com/example/PaginaWebDivisas/PaginaWebDivisasApplicationTests.java
+++ PaginaWebDivisas/src/test/java/com/example/PaginaWebDivisas/PaginaWebDivisasApplicationTests.java
@@ -2,11 +2,10 @@ package com.example.PaginaWebDivisas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class PaginaWebDivisasApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
